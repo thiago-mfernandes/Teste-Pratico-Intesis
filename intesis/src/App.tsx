e53490daafb0101d1a3ee3971e2867b205694604
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-10-20 10:31:42 -0300

    criacao do app, limpeza e arquivo de estilo global

---
+++ intesis/src/App.tsx
@@ -0,0 +1,13 @@
+//import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
+//const Title = styled.h1``;
+
+export default function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>hello world!</h1>
+    </div>
+  );
+}
