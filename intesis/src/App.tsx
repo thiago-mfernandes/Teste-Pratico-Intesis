e9ef042df88420498496ce16c804a2d5185634e1
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-10-21 07:56:32 -0300

    roteamento

--- intesis/src/App.tsx
+++ intesis/src/App.tsx
@@ -2,33 +2,19 @@ import { Formulario } from './components/Formulario';
 import { Header } from './components/Header';
 import { TabelaClientes } from './components/TabelaClientes';
 import { GlobalStyle } from './styles/global';
-import { MagnifyingGlass, PlusCircle } from 'phosphor-react';
-
-import { Container, BotaoAdicionar, ContainerSearch } from './styles';
-
+import {  BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <Header />
-
-      <Container>
-        <BotaoAdicionar>
-          <PlusCircle size={20}/>
-          Cliente
-        </BotaoAdicionar> {/**enviar para cadastro */}
-
-        <ContainerSearch>
-          <label htmlFor="search">
-            <MagnifyingGlass color='#2873B6' size={20}/>
-          </label>
-          <input type="search" id="search"/>
-        </ContainerSearch>  
-      </Container>
-
-      {/*<TabelaClientes /> */}
-      <Formulario />
-    </div>
+    <>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path='/' element={ <TabelaClientes /> }/>
+          <Route path='novoCliente' element={ <Formulario /> } />          
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
--- intesis/src/components/TabelaClientes/index.tsx
+++ intesis/src/components/TabelaClientes/index.tsx
@@ -1,80 +1,99 @@
-import { Table } from './styles';
-import { PencilSimple, UserMinus, ArrowUp } from 'phosphor-react';
+import { Table, Container, BotaoAdicionar, ContainerSearch } from './styles';
+import { PencilSimple, UserMinus, ArrowUp, PlusCircle, MagnifyingGlass } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 
 export function TabelaClientes() {
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nome/Razão Social <ArrowUp size={18}/></th>
-          <th>CPF/CNPJ</th>
-          <th>Email</th>
-          <th>Tel</th>
-          <th>Cel</th>
-          <th>{' '}</th>
-          <th>{' '}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Cerri & Santos Equipamentos</td>
-          <td>23.123.456/0001-01</td>
-          <td>assistech19@gmail.com</td>
-          <td>19 3456-7890</td>
-          <td>19 98765-4321</td>
-          <td>
-            <button>
-              <PencilSimple color='#2873B6'/>
-            </button>
-          </td>
-          <td>
-            <button>
-            <UserMinus color='#EA3F7A'/>
-            </button>
-          </td>
-        </tr>    
+    <>
+      <Container>
+        <Link to="/novoCliente">
+          <BotaoAdicionar>
+            <PlusCircle size={20}/>
+            Cliente
+          </BotaoAdicionar>
+        </Link>
 
-        <tr>
-          <td>1</td>
-          <td>Cerri & Santos Equipamentos</td>
-          <td>23.123.456/0001-01</td>
-          <td>assistech19@gmail.com</td>
-          <td>19 3456-7890</td>
-          <td>19 98765-4321</td>
-          <td>
-            <button>
-              <PencilSimple color='#2873B6'/>
-            </button>
-          </td>
-          <td>
-            <button>
-            <UserMinus color='#EA3F7A'/>
-            </button>
-          </td>
-        </tr>
+        <ContainerSearch>
+          <label htmlFor="search">
+            <MagnifyingGlass color='#2873B6' size={20}/>
+          </label>
+          <input type="search" id="search"/>
+        </ContainerSearch>  
+      </Container>
 
-        <tr>
-          <td>1</td>
-          <td>Cerri & Santos Equipamentos</td>
-          <td>23.123.456/0001-01</td>
-          <td>assistech19@gmail.com</td>
-          <td>19 3456-7890</td>
-          <td>19 98765-4321</td>
-          <td>
-            <button>
-              <PencilSimple color='#2873B6'/>
-            </button>
-          </td>
-          <td>
-            <button>
-            <UserMinus color='#EA3F7A'/>
-            </button>
-          </td>
-        </tr>
-        
-      </tbody>
-    </Table>
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome/Razão Social <ArrowUp size={18}/></th>
+            <th>CPF/CNPJ</th>
+            <th>Email</th>
+            <th>Tel</th>
+            <th>Cel</th>
+            <th>{' '}</th>
+            <th>{' '}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Cerri & Santos Equipamentos</td>
+            <td>23.123.456/0001-01</td>
+            <td>assistech19@gmail.com</td>
+            <td>19 3456-7890</td>
+            <td>19 98765-4321</td>
+            <td>
+              <button>
+                <PencilSimple color='#2873B6'/>
+              </button>
+            </td>
+            <td>
+              <button>
+              <UserMinus color='#EA3F7A'/>
+              </button>
+            </td>
+          </tr>    
+
+          <tr>
+            <td>1</td>
+            <td>Cerri & Santos Equipamentos</td>
+            <td>23.123.456/0001-01</td>
+            <td>assistech19@gmail.com</td>
+            <td>19 3456-7890</td>
+            <td>19 98765-4321</td>
+            <td>
+              <button>
+                <PencilSimple color='#2873B6'/>
+              </button>
+            </td>
+            <td>
+              <button>
+              <UserMinus color='#EA3F7A'/>
+              </button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>1</td>
+            <td>Cerri & Santos Equipamentos</td>
+            <td>23.123.456/0001-01</td>
+            <td>assistech19@gmail.com</td>
+            <td>19 3456-7890</td>
+            <td>19 98765-4321</td>
+            <td>
+              <button>
+                <PencilSimple color='#2873B6'/>
+              </button>
+            </td>
+            <td>
+              <button>
+              <UserMinus color='#EA3F7A'/>
+              </button>
+            </td>
+          </tr>
+          
+        </tbody>
+      </Table>
+    </>
   );
 }
